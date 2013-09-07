945e1f7c06599ba4a9d6b601889824d06635e1ad
KenichiroHirose <hirose@infodex.co.jp>
2013-09-07 20:32:31 +0900

    Twilio Auth Tokenを環境変数から読むように変更

--- send_message.php
+++ send_message.php
@@ -7,7 +7,7 @@ require_once('/Services/Twilio.php'); // Loads the library
 
 // Your Account Sid and Auth Token from twilio.com/user/account
 $sid = "ACb6a2a22711d50f770902b427997bfbb9"; 
-$token = "2e127cf484b49aad024477fb605060b5"; 
+$token = getenv("TWILIO_AUTH_TOKEN"); 
 $client = new Services_Twilio($sid, $token);
  
 $message = $client->account->sms_messages->create("+12077473988", $To, $Message, array());
